47a2b2bd1cb543b729533b1491df141a73245406
ALEXANDER PLETZER <axp5530@psu.edu>
2015-12-03 07:07:16 -0700

    no longer breaking long lines

--- tools/icqsol_rotate_shape/icqsol_rotate_shape.py
+++ tools/icqsol_rotate_shape/icqsol_rotate_shape.py
@@ -9,21 +9,13 @@ import numpy
 
 # Parse Command Line.
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', dest='input',
-                    help='Shape dataset selected from history')
-parser.add_argument('--rotation_angle_degrees',
-                    dest='rotation_angle_degrees', type=float,
-                    default=0.0, help='Rotation angle in degrees')
-parser.add_argument('--rotation_axis_x', dest='rotation_axis_x', type=float,
-                    default=1.0, help='X coordinate of rotation axis')
-parser.add_argument('--rotation_axis_y', dest='rotation_axis_y', type=float,
-                    default=0.0, help='Y coordinate of rotation axis')
-parser.add_argument('--rotation_axis_z', dest='rotation_axis_z', type=float,
-                    default=0.0, help='Z coordinate of rotation axis')
+parser.add_argument('--input', dest='input', help='Shape dataset selected from history')
+parser.add_argument('--rotation_angle_degrees', dest='rotation_angle_degrees', type=float, default=0.0, help='Rotation angle in degrees')
+parser.add_argument('--rotation_axis_x', dest='rotation_axis_x', type=float, default=1.0, help='X coordinate of rotation axis')
+parser.add_argument('--rotation_axis_y', dest='rotation_axis_y', type=float, default=0.0, help='Y coordinate of rotation axis')
+parser.add_argument('--rotation_axis_z', dest='rotation_axis_z', type=float, default=0.0, help='Z coordinate of rotation axis')
 parser.add_argument('--output', dest='output', help='Output file name')
-parser.add_argument('--output_vtk_type', dest='output_vtk_type',
-                    default='ascii',
-                    help='Output VTK type')
+parser.add_argument('--output_vtk_type', dest='output_vtk_type', default='ascii', help='Output VTK type')
 
 args = parser.parse_args()
 
@@ -39,8 +31,7 @@ file_format = util.getFileFormat(args.input)
 if file_format == util.VTK_FORMAT:
     # We have a VTK file, so get the dataset type.
     vtk_dataset_type = util.getVtkDatasetType(args.input)
-    shape_mgr = ShapeManager(file_format=file_format,
-                             vtk_dataset_type=vtk_dataset_type)
+    shape_mgr = ShapeManager(file_format=file_format, vtk_dataset_type=vtk_dataset_type)
 else:
     shape_mgr = ShapeManager(file_format=file_format)
 
@@ -48,16 +39,12 @@ else:
 vtk_poly_data = shape_mgr.loadAsVtkPolyData(args.input)
 
 # Only rotate if the axis has some non-zero coordinates.
-axis = numpy.array((args.rotation_axis_x,
-                    args.rotation_axis_y,
-                    args.rotation_axis_z))
+axis = numpy.array((args.rotation_axis_x, args.rotation_axis_y, args.rotation_axis_z))
 
 if axis.dot(axis) != 0.0:
 
     # Rotate (in place operation).
-    shape_mgr.rotateVtkPolyData(vtk_poly_data,
-                                angleDeg=args.rotation_angle_degrees,
-                                axis=axis)
+    shape_mgr.rotateVtkPolyData(vtk_poly_data, angleDeg=args.rotation_angle_degrees, axis=axis)
 
 if args.output_vtk_type.lower() == 'binary':
     file_type = util.BINARY
@@ -67,7 +54,6 @@ else:
 tmp_dir = icqsol_utils.get_temp_dir()
 tmp_output_path = icqsol_utils.get_temporary_file_path(tmp_dir, 'vtk')
 
-shape_mgr.saveVtkPolyData(vtk_poly_data, file_name=tmp_output_path,
-                          file_type=file_type)
+shape_mgr.saveVtkPolyData(vtk_poly_data, file_name=tmp_output_path, file_type=file_type)
 
 shutil.move(tmp_output_path, args.output)
--- tools/icqsol_translate_shape/icqsol_translate_shape.py
+++ tools/icqsol_translate_shape/icqsol_translate_shape.py
@@ -8,18 +8,12 @@ import icqsol_utils
 
 # Parse Command Line.
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', dest='input',
-                    help='Shape dataset selected from history')
-parser.add_argument('--displacement_x', dest='displacement_x', type=float,
-                    default=1.0, help='X coordinate of displacement')
-parser.add_argument('--displacement_y', dest='displacement_y', type=float,
-                    default=0.0, help='Y coordinate of displacement')
-parser.add_argument('--displacement_z', dest='displacement_z', type=float,
-                    default=0.0, help='Z coordinate of displacement')
+parser.add_argument('--input', dest='input', help='Shape dataset selected from history')
+parser.add_argument('--displacement_x', dest='displacement_x', type=float, default=1.0, help='X coordinate of displacement')
+parser.add_argument('--displacement_y', dest='displacement_y', type=float, default=0.0, help='Y coordinate of displacement')
+parser.add_argument('--displacement_z', dest='displacement_z', type=float, default=0.0, help='Z coordinate of displacement')
 parser.add_argument('--output', dest='output', help='Output file name')
-parser.add_argument('--output_vtk_type', dest='output_vtk_type',
-                    default='ascii',
-                    help='Output VTK type')
+parser.add_argument('--output_vtk_type', dest='output_vtk_type', default='ascii', help='Output VTK type')
 
 args = parser.parse_args()
 
@@ -35,21 +29,17 @@ file_format = util.getFileFormat(args.input)
 if file_format == util.VTK_FORMAT:
     # We have a VTK file, so get the dataset type.
     vtk_dataset_type = util.getVtkDatasetType(args.input)
-    shape_mgr = ShapeManager(file_format=file_format,
-                             vtk_dataset_type=vtk_dataset_type)
+    shape_mgr = ShapeManager(file_format=file_format, vtk_dataset_type=vtk_dataset_type)
 else:
     shape_mgr = ShapeManager(file_format=file_format)
 
 # Get the vtk polydata from the input dataset.
 vtk_poly_data = shape_mgr.loadAsVtkPolyData(args.input)
 
-displ = (args.displacement_x,
-         args.displacement_y,
-         args.displacement_z)
+displ = (args.displacement_x, args.displacement_y, args.displacement_z)
 
 # Translate (in place operation).
-shape_mgr.translateVtkPolyData(vtk_poly_data,
-                               displ=displ)
+shape_mgr.translateVtkPolyData(vtk_poly_data, displ=displ)
 
 if args.output_vtk_type.lower() == 'binary':
     file_type = util.BINARY
@@ -59,7 +49,6 @@ else:
 tmp_dir = icqsol_utils.get_temp_dir()
 tmp_output_path = icqsol_utils.get_temporary_file_path(tmp_dir, 'vtk')
 
-shape_mgr.saveVtkPolyData(vtk_poly_data, file_name=tmp_output_path,
-                          file_type=file_type)
+shape_mgr.saveVtkPolyData(vtk_poly_data, file_name=tmp_output_path, file_type=file_type)
 
 shutil.move(tmp_output_path, args.output)
